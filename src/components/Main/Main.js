1d877ba8f2c3747a5a3f6f02bb0e1e734eab12c7
Kevin Wessa <kjwessa@gmail.com>
2023-10-08 12:39:57 -0500

    feat: add multiple upcoming images to the slider

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,41 +3,77 @@ import React from "react";
 function Main({ mainPhoto, nextPhoto, prevPhoto, onNextClick, onPrevClick }) {
   return (
     <main className="main">
-      {/* Display the main photo */}
       {mainPhoto && (
-        <div className="main__photo-container">
-          <img
-            src={mainPhoto.urls.regular}
-            alt={mainPhoto.alt_description}
-            className="main__photo"
-          />
-        </div>
+        <img className="main__image" src={mainPhoto.urls.full} alt={mainPhoto.alt_description} />
       )}
 
-      {/* Display navigation buttons */}
-      <div className="main__controls">
-        {prevPhoto && (
-          <button className="main__button main__button--prev" onClick={onPrevClick}>
-            Previous
-          </button>
-        )}
-        {nextPhoto && (
-          <button className="main__button main__button--next" onClick={onNextClick}>
-            Next
-          </button>
-        )}
+      <div className="main__navigation">
+        <div
+          className={`main__button main__button--previous ${
+            !prevPhoto ? "main__button--inactive" : ""
+          }`}
+          onClick={onPrevClick}>
+          {/* SVG for left arrow */}
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M15 18L9 12L15 6"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+
+        <div
+          className={`main__button main__button--next ${
+            !nextPhoto ? "main__button--inactive" : ""
+          }`}
+          onClick={onNextClick}>
+          {/* SVG for right arrow */}
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M9 18L15 12L9 6"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
       </div>
 
-      {/* Display a small thumbnail of the next photo at the bottom */}
-      {nextPhoto && (
-        <div className="main__thumbnail">
+      <div className="main__thumbnails">
+        {prevPhoto && (
           <img
-            src={nextPhoto.urls.thumb}
-            alt={nextPhoto.alt_description}
-            className="main__thumbnail-image"
+            className="main__thumbnail"
+            src={prevPhoto.urls.small}
+            alt={prevPhoto.alt_description}
           />
-        </div>
-      )}
+        )}
+        <img
+          className="main__thumbnail main__thumbnail--active"
+          src={mainPhoto.urls.small}
+          alt={mainPhoto.alt_description}
+        />
+        {nextPhoto && (
+          <img
+            className="main__thumbnail"
+            src={nextPhoto.urls.small}
+            alt={nextPhoto.alt_description}
+          />
+        )}
+      </div>
     </main>
   );
 }
