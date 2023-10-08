0fc44c7427d9950252fefb9c5d4ccb9dc496e54f
Kevin Wessa <kjwessa@gmail.com>
2023-10-08 12:17:23 -0500

    git status clear :

---
+++ src/utils/api.js
@@ -0,0 +1,29 @@
+import { APIKey, baseUrl } from "./constants";
+
+const requestOptions = {
+  headers: {
+    Authorization: `Client-ID ${APIKey}`,
+    Accept: "application/json",
+  },
+};
+
+export async function getPhotos() {
+  console.log("Initiating API call to fetch photos...");
+
+  try {
+    const response = await fetch(`${baseUrl}photos?per_page=10`, requestOptions);
+
+    if (!response.ok) {
+      console.error(`API call error. Status: ${response.status}`);
+      throw new Error("Failed to fetch photos from Unsplash API");
+    }
+
+    const data = await response.json();
+    console.log("Photos successfully retrieved from API:", data);
+
+    return data;
+  } catch (error) {
+    console.error("Error during the API call:", error);
+    throw error;
+  }
+}
